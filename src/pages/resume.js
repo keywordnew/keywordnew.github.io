3b62433c09cb20d910bd4086b7bbb7cef2baa7ca
Chowdhurian <manil.chowdhury@gmail.com>
2018-04-24 12:40:22 -0700

    Update resume links

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -12,12 +12,12 @@ export default () =>
   <Main>
     <Resume
       name="view pdf"
-      url="https://www.dropbox.com/s/pcgqa3n1kxfswzb/manil_chowdhury.pdf?dl=0"
+      url="https://drive.google.com/open?id=1Z3G52y1qNkvHGwUBkJheyoCFQ_-43_2s"
     />
 
     <Resume
       name="download pdf"
-      url="https://www.dropbox.com/s/pcgqa3n1kxfswzb/manil_chowdhury.pdf?dl=1"
+      url="https://doc-0c-0c-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/hb2pq2kr7vp0vbnei84g6fkv35ruj62q/1524592800000/08430212430750148255/*/1Z3G52y1qNkvHGwUBkJheyoCFQ_-43_2s?e=download"
     />
 
     <Resume
