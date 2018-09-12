b2e70ca1b71bb534cb8a707bbcc93ebbd1a149e7
Manil Chowdhury <manil.chowdhury@gmail.com>
2018-09-12 11:42:28 -0700

    Make activity component

---
+++ src/components/activity.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export const Activity = props => (
+  <div>
+    <a href={props.url}>{props.name}</a>
+  </div>
+)
+
+Activity.propTypes = {
+  url: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+}
+
+export default Activity
