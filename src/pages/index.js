33637dacd7237c811b0eae61ae5faefd26b602ec
Manil Chowdhury <manil.chowdhury@gmail.com>
2021-08-21 16:31:27 -0700

    Update front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,10 @@ const ListLink = props => (
 const IndexPage = () => (
   <Main>
     <p>
-      Developer advocate and software engineer, <Link to="/writing/">writing about finding calm on the web</Link>, creating <Outlink name='products for meatspace' url='https://www.womburtle.com/' />, and building <Link to="/communities/">communities</Link>. ex<Outlink name='-InVision' url='https://www.crunchbase.com/organization/invisionapp' />, <Outlink name='-Edvisor' url='https://www.crunchbase.com/organization/edvisor-io' /> <a target='_blank' rel='noopener noreferrer' href='' target='_blank'></a>, and <Outlink name='-UBC' url='https://www.crunchbase.com/organization/university-of-british-columbia' />. I'm <Link to='/living/'>living</Link> in Vancouver BC 🇨🇦 and <Link to="/resume/">working</Link> with remote/distributed teams.
+      Developer advocate and software engineer. <Outlink name='Making remote 
+      easy for entrepreneurs' url='https://majorcord.com/' />, <Link 
+      to="/writing/">writing</Link> and <Outlink name='hanging' 
+      url='https://talk.paperstrawbookclub.com/' /> on the web, creating <Outlink name='products for meatspace' url='https://www.womburtle.com/' />, and building <Link to="/communities/">communities</Link>. ex<Outlink name='-InVision' url='https://www.crunchbase.com/organization/invisionapp' />, <Outlink name='-Edvisor' url='https://www.crunchbase.com/organization/edvisor-io' />, and <Outlink name='-UBC' url='https://www.crunchbase.com/organization/university-of-british-columbia' />. I'm <Link to='/living/'>living</Link> in Vancouver BC 🇨🇦 and <Link to="/resume/">working</Link> with remote/distributed teams.
     </p>
     <Link to="/contact/">Get in touch.</Link>
   </Main>
